40c24fb542a15438af69760eb13d1270abc8da64
Maija <maijaandersone@inbox.lv>
2023-04-13 20:00:21 +0300

    Login working progress

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -1,14 +1,29 @@
 import { config } from 'dotenv';
 import express from 'express';
+import { urlencoded } from 'body-parser';
 
 config();
 
-async function createServer() {
+
+
+    function createServer() {
     const app = express();
+    app.use(urlencoded())
+
     const port = process.env.PORT;
 
-    app.get('/', (req, res) => {
-        res.send('Hello world from back end');
+    app.get('/',(req, res, next) => {
+        res.send(`<form method="POST" action="/login">
+        <div>Email: <input type="text" name="email" /></div>
+        <div>Password: <input type="password" name="password"/> </div>
+        <div><input type="submit" value="Submit" /></div>
+        </form>`);
+    });
+
+    app.post('/login', (req, res) => {
+        const data = req.body;
+        console.log(data)
+        res.send(data);
     });
 
     app.listen(port, () => {
