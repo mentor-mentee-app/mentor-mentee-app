cf061691daf87ce17a78e26e48cc3aee201e6edd
Davis Bojars <2466545+bojars@users.noreply.github.com>
2023-05-08 19:52:17 +0300

    Work in progress login with cookie

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import { json } from 'body-parser'; 
 
@@ -9,18 +10,32 @@ import { login } from './services/login'
 
 function createServer() {
     const app = express();
-    app.use(cors());
+    app.use(cors({
+        origin: 'http://127.0.0.1:5173',
+        optionsSuccessStatus: 200,
+        credentials: true
+    }));
     app.use(json());
+
+    app.set('trust proxy', 1) // trust first proxy
+    app.use(session({
+        secret: 'mans noslēpums',
+        resave: false,
+        saveUninitialized: true,
+    }))
 
     const port = process.env.PORT;
 
     app.get('/', (req, res, next) => {
-        res.send(`BE root`);
+        res.send(`BE root` + /* req.session.loggedIn */);
     });
 
     app.post('/login', async (req, res) => {
         const data = req.body;
-        const result = await login(data.email, data.password)
+        const result = await login(data.email, data.password);
+
+        /* req.session.loggedIn = true; */
+
         if (result) {
             res.send({ success: true })
         } else {
