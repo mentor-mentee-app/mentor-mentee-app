76603b4a658bcf7222946339327299db9006ff25
Davis Bojars <2466545+bojars@users.noreply.github.com>
2023-05-22 19:41:22 +0300

    Add /logout and /me routes

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -2,15 +2,13 @@ import { config } from 'dotenv';
 import express from 'express';
 import session from 'express-session';
 import cors from 'cors';
-import { json } from 'body-parser'; 
+import { json } from 'body-parser';
 
 config();
 
 import { login } from './services/login'
 
 function createServer() {
-    
-
     const feURL = process.env.FE_URL;
 
     const app = express();
@@ -40,17 +38,30 @@ function createServer() {
         const data = req.body;
         const result = await login(data.email, data.password);
 
-       
-
         if (result) {
-           
             req.session['loggedIn'] = true;
             req.session['email'] = data.email;
+
             res.send({ success: true })
         } else {
+            req.session['loggedIn'] = false;
+
             res.send({ success: false })
         }
-    })
+    });
+
+    app.get('/logout', async (req, res) => {
+        req.session.destroy(() => {
+            res.redirect(`${feURL}`);
+        });
+    });
+
+    app.get('/me', async (req, res, next) => {
+        res.json({
+            loggedIn: req.session['loggedIn'] ?? false,
+            email: req.session['email']
+        });
+    });
 
     app.listen(port, () => {
         console.log(`[server]: Server is running at http://localhost:${port}`);
